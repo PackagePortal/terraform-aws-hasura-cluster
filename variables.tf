64eec1bf5166204ae0302532e86981c1b2f46bd3
bjewkes <33266674+bjewkes@users.noreply.github.com>
2026-05-06 11:12:00 -0400

    Ephemeral volumes for readonly file systems (#11)
    * Add ephemeral volumes for readonly file systems to have a mounted volume in /tmp
    
    * Remove condition - fails validation when null

--- variables.tf
+++ variables.tf
@@ -150,6 +150,12 @@ variable "readonly_root_filesystem" {
   default     = false
 }
 
+variable "task_ephemeral_storage_size_in_gib" {
+  description = "Optional Fargate task ephemeral storage size in GiB. Set this to increase writable scratch storage beyond the default 20 GiB."
+  type        = number
+  default     = null
+}
+
 ###########################
 # Network related variables
 ###########################
